783c8f29750c6b5f2c1fafd6056a6a9df8563abd
kallaugher <alicekallaugher@gmail.com>
2016-10-04 10:53:49 -0400

    add MaterialUI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import NavBar from './nav_bar';
+// import NavBar from './nav_bar';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
+
+const style = {
+  height: 800,
+  width: 400,
+  margin: 10,
+  textAlign: 'center',
+  display: 'inline-block',
+};
 
 export default (props) => {
    return (
-     <div>
-       <NavBar title="houseMate" url='/' />
-      <h1> Welcome! </h1>
-     </div>
+      <MuiThemeProvider>
+       <div>
+         <AppBar
+          title="houseMate"
+          url='/'
+          style={{
+            fontFamily: 'Pacifico'
+          }}/>
+        <h1> Welcome! </h1>
+        <Paper style={style} zDepth={2} >
+          {"Hello!"}
+        </Paper>
+        <Paper style={style} zDepth={2} >
+          {"Hello!"}
+        </Paper>
+       </div>
+      </MuiThemeProvider>
    )
  }
--- src/index.js
+++ src/index.js
@@ -6,8 +6,14 @@ import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+//http://stackoverflow.com/a/34015469/988941
+
+injectTapEventPlugin();
 ReactDOM.render(
   <Router history={browserHistory}
    routes={routes} />,
   document.getElementById('root')
+
 );
